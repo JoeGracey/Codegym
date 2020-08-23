3a4ced5f98cf2e43ad7b2dfee0bb50f8ed9d4b01
Joe <joe.gracey@gmail.com>
2020-08-22 21:41:14 -0600

    FirstCommit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    /*
+	    Create the Dog class. The dog should have a String name and int age.
+	    Create getters and setters for all the variables of the Dog class.
+
+	    Requirements:
+
+	    - The program must not read data from the keyboard.
+	    - The Dog class must have a String variable name.
+	    - The Dog class must have an int variable age.
+	    - The class must have a getter for the variable name and variable age.
+	    - The class must have a setter for the variable name and variable age.
+
+	    This is the code that I am given with the assignment:
+
+	    public class Dog {
+
+	        public static void main(String[] args) {
+
+	        }
+	    }
+	     */
+    }
+}
