f2c3eec4ba3aa4365d153dff9075095841999ebf
Joe Gracey <joe.gracey@gmail.com>
2020-08-30 21:05:04 -0600

    Add files via upload

---
+++ TheThreeMusCATeers/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    class Cat {
+	        private String name;
+	        private int age;
+	        private int weight;
+	        private int strength;
+
+	        public Cat(String name, int age, int weight, int strength) {
+	            this.name = name;
+	            this.age = age;
+	            this.weight = weight;
+	            this.strength = strength;
+            }
+        }
+    }
+}
+
+//In the main method, create three Cat objects and fill them with data.
+//Use the Cat class from the first task.
+//You don't need to create the Cat class.
+
+//Requirements:
+//1. The program must not read data from the keyboard.
+//2. You need to create three Cat objects.
+//3. Don't change the Cat class.
+//4. The program should not display data on the screen.
