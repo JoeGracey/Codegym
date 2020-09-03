f619c4596ed487f1abe1f8d23ba9215ae9b55229
Joe Gracey <joe.gracey@gmail.com>
2020-09-02 22:40:19 -0600

    first commit

---
+++ People/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    /*
+	    Create the Person class. A person should have a String name, int age, String address, and
+	     char sex.
+
+	     Requirements:
+	     1. The program must not read data from the keyboard.
+	     2. The Person class must have an int variable age.
+	     3. The Person class must have a String variable address.
+	     4. The Person class must have a String name.
+	     5. The Person class must have a char variable sex.
+	     6. There must be 4 variables in the class.
+	     */
+    }
+
+    public class Person {
+        // write your code here
+        String name = "Joe";
+        int age = 41;
+        String address = "2663 W Titans Court";
+        char sex = 'M';
+    }
+}
